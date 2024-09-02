853f8bcd842f9843253ceab179b0a61489a50a99
pratik-codes <pratik2018id@gmail.com>
2024-09-02 19:34:32 +0530

    footer changes

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -12,26 +12,26 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Icons.logo />
           <span className="text-lg font-bold">SIFU{" "}(Safe Interactions for Users)</span>
-          <p className="text-center text-sm leading-loose md:text-left">
-            Built by{" "}
-            <a
-              href={"https://tiwaripratik.com"}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-             Pratik{" "}
-            </a>
-             &{" "}
-            <a
-              href={"https://github.com/RishiGitH"}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-             Rishi
-            </a>
-          </p>
+          {/* <p className="text-center text-sm leading-loose md:text-left"> */}
+          {/*   Built by{" "} */}
+          {/*   <a */}
+          {/*     href={"https://tiwaripratik.com"} */}
+          {/*     target="_blank" */}
+          {/*     rel="noreferrer" */}
+          {/*     className="font-medium underline underline-offset-4" */}
+          {/*   > */}
+          {/*    Pratik{" "} */}
+          {/*   </a> */}
+          {/*    &{" "} */}
+          {/*   <a */}
+          {/*     href={"https://github.com/RishiGitH"} */}
+          {/*     target="_blank" */}
+          {/*     rel="noreferrer" */}
+          {/*     className="font-medium underline underline-offset-4" */}
+          {/*   > */}
+          {/*    Rishi */}
+          {/*   </a> */}
+          {/* </p> */}
         </div>
         {/* <ModeToggle /> */}
       </div>
