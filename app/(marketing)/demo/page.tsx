75243807d982496684d491aca137622e6bb2d81f
pratik-codes <pratik2018id@gmail.com>
2024-10-09 01:09:57 +0530

    fix: typo

--- app/(marketing)/demo/page.tsx
+++ app/(marketing)/demo/page.tsx
@@ -5,23 +5,23 @@ import React from 'react';
 import SolanaPool from './components/solana-pool';
 
 const DemoPage = () => {
-const vulenrablePoolData = {
-  mainTitle: "Solana Vunerable Pool",
-  mainDescription: "This pool is not secured by SIFU",
-  contractAddress: "24ysSjyaAdFYTCN9WabADrVkipaTCvv97xvuLSC2y7Bk",
-  poolType: "vulnerable",
-}
+  const vulenrablePoolData = {
+    mainTitle: "Solana Vulnerable Pool",
+    mainDescription: "This pool is not secured by SIFU",
+    contractAddress: "24ysSjyaAdFYTCN9WabADrVkipaTCvv97xvuLSC2y7Bk",
+    poolType: "vulnerable",
+  }
 
-const sifuSecuredPoolData = {
-  mainTitle: "Solana Vunerable SIFU Secured Pool",
-  mainDescription: "This pool is secured by SIFU",
-  contractAddress: "DE8mBCGgZi8aFHfXvdsjWUFWb8MJnZv8SFxvA8xzUVpt",
-  poolType: "sifu",
-}
+  const sifuSecuredPoolData = {
+    mainTitle: "Solana Vulnerable SIFU Secured Pool",
+    mainDescription: "This pool is secured by SIFU",
+    contractAddress: "DE8mBCGgZi8aFHfXvdsjWUFWb8MJnZv8SFxvA8xzUVpt",
+    poolType: "sifu",
+  }
 
   return (
     <div className='h-screen w-screen flex justify-center items-center'>
-      <SolanaPool {...vulenrablePoolData}  />
+      <SolanaPool {...vulenrablePoolData} />
       <SolanaPool {...sifuSecuredPoolData} />
     </div>
   )
