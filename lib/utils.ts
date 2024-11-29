136e722109cd1fca64f2bf984cd038192cbfa1a5
pratik-codes <pratik2018id@gmail.com>
2024-11-30 00:25:25 +0530

    changed utc times to 5 days

--- lib/utils.ts
+++ lib/utils.ts
@@ -66,7 +66,7 @@ export const getTimeAgo = (timestamp: string) => {
 export const getTransactionTimestamp = (): string => {
   const now = new Date();
 
-  now.setUTCDate(now.getUTCDate() - 1);
+  now.setUTCDate(now.getUTCDate() - 5);
 
   const year = now.getUTCFullYear();
   const month = String(now.getUTCMonth() + 1).padStart(2, '0'); // Months are zero-indexed
